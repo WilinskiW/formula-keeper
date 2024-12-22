e8a6b328afda09307a0290e031f8ce6bb6a3062a
wilin <wilinskwiktor27@gmail.com>
2024-12-22 17:27:57 +0100

    Initial commit

---
+++ latex-parser/src/main/java/com/wilinskiw/portfolio/latex_parser/LatexParserApplication.java
@@ -0,0 +1,13 @@
+package com.wilinskiw.portfolio.latex_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatexParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatexParserApplication.class, args);
+	}
+
+}
