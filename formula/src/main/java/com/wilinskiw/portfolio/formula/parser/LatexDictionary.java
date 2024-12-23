775b334a11f1fbeb4b9083e5dab8619218f3661e
wilin <wilinskwiktor27@gmail.com>
2024-12-23 13:33:54 +0100

    Add to MathUtils: sin, cos, tan to minimalize value in LatexDictionary

--- formula/src/main/java/com/wilinskiw/portfolio/formula/parser/LatexDictionary.java
+++ formula/src/main/java/com/wilinskiw/portfolio/formula/parser/LatexDictionary.java
@@ -10,9 +10,9 @@ public enum LatexDictionary {
     LOG10("\\\\log(\\d+)", "T(java.lang.Math).log10($1)"),
     DIGIT_BASE_LOG("\\\\log\\_(\\d)(\\d+)", "#mathUtils.logBase($1,$2)"),
     NUMBER_BASE_LOG("\\\\log\\_\\{([^}]+)}(\\d+)", "#mathUtils.logBase($1,$2)"),
-    SIN("\\\\sin(\\d+)", "T(java.lang.Math).sin(T(java.lang.Math).toRadians($1))"),
-    COS("\\\\cos(\\d+)", "T(java.lang.Math).cos(T(java.lang.Math).toRadians($1))"),
-    TAN("\\\\tan(\\d+)", "T(java.lang.Math).tan(T(java.lang.Math).toRadians($1))"),
+    SIN("\\\\sin(\\d+)", "#mathUtils.sin($1)"),
+    COS("\\\\cos(\\d+)", "#mathUtils.cos($1)"),
+    TAN("\\\\tan(\\d+)", "#mathUtils.tan($1)"),
     PI("\\\\pi", "T(java.lang.Math).PI"),
     ROUND_BRACKETS("\\\\left\\(([^)]+)\\\\right\\)", "($1)"),
     PERCENT("(\\d+)\\\\%", "$1 * 0.01");
--- formula/src/main/java/com/wilinskiw/portfolio/formula/utils/MathUtils.java
+++ formula/src/main/java/com/wilinskiw/portfolio/formula/utils/MathUtils.java
@@ -19,4 +19,16 @@ public class MathUtils {
 
         return Math.log(value) / Math.log(base);
     }
+
+    public double sin(double angle) {
+        return Math.sin(Math.toRadians(angle));
+    }
+
+    public double cos(double angle) {
+        return Math.cos(Math.toRadians(angle));
+    }
+
+    public double tan(double angle) {
+        return Math.tan(Math.toRadians(angle));
+    }
 }
