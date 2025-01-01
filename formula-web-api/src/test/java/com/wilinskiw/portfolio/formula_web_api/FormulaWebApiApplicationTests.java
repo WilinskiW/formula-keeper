d60a0b00cdce876a5a136c0cf3086428ccd74e55
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-01 21:49:27 +0100

    intialize spring boot project package

---
+++ formula-web-api/src/test/java/com/wilinskiw/portfolio/formula_web_api/FormulaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilinskiw.portfolio.formula_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
