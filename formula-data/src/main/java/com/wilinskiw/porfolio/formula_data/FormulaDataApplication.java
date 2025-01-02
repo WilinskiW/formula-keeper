beae1aa9c0f9cca5d41f50eb2bdca44b9fd86eaf
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-02 17:13:56 +0100

    export spring boot formula-data project

---
+++ formula-data/src/main/java/com/wilinskiw/porfolio/formula_data/FormulaDataApplication.java
@@ -0,0 +1,13 @@
+package com.wilinskiw.porfolio.formula_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaDataApplication.class, args);
+	}
+
+}
---
+++ formula-data/src/test/java/com/wilinskiw/porfolio/formula_data/FormulaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilinskiw.porfolio.formula_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
