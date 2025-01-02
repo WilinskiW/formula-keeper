3f73ee310d10c13424b54e74b36b38a48549f9f6
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-02 15:46:32 +0100

    view input latex, mathJax latex and evaluated formula

--- formula-web-api/src/main/java/com/wilinskiw/portfolio/formula_web_api/controller/FormulaController.java
+++ formula-web-api/src/main/java/com/wilinskiw/portfolio/formula_web_api/controller/FormulaController.java
@@ -1,12 +1,21 @@
 package com.wilinskiw.portfolio.formula_web_api.controller;
 
+import com.wilinskiw.portfolio.formula_web_api.service.FormulaWebService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormulaController {
+    private final FormulaWebService webService;
+
+    @Autowired
+    public FormulaController(FormulaWebService webService) {
+        this.webService = webService;
+    }
 
     @GetMapping("/form")
     public String viewForm() {
@@ -14,7 +23,8 @@ public class FormulaController {
     }
 
     @PostMapping("/form")
-    public String getFormula(@RequestParam("latex") String mathField) {
-        return "redirect:/form";
+    public String getResult(@RequestParam("latex") String mathField, Model model) {
+        model.addAttribute("result", webService.parseFormula(mathField));
+        return "formula-result";
     }
 }
