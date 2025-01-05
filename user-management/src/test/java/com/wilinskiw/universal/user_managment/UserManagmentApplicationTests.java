5406ba0d062e8a30ca9265336c6e923ae3cc96c3
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-05 15:07:27 +0100

    intialize user-management module

---
+++ user-management/src/test/java/com/wilinskiw/universal/user_managment/UserManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilinskiw.universal.user_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
